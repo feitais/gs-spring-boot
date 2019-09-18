ca9879a83c0703ce7c4dcea6e6df006cd06dc55c
feitais <feitais@users.noreply.github.com>
2019-09-18 15:37:25 -0300

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
     	
     	FileHandler fh;  
     	// This block configure the logger with handler and formatter  
-        fh = new FileHandler("console.log");  
+        fh = new FileHandler("/logs/console.log");  
         logger.addHandler(fh);
         SimpleFormatter formatter = new SimpleFormatter();  
         fh.setFormatter(formatter);  
