558133abf44a007115b7fe6559b8540e93cecb38
feitais <feitais@users.noreply.github.com>
2019-09-18 15:32:59 -0300

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,32 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index() throws SecurityException, IOException {
+    	Logger logger = Logger.getLogger("MyLog");  
+    	
+    	FileHandler fh;  
+    	// This block configure the logger with handler and formatter  
+        fh = new FileHandler("console.log");  
+        logger.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();  
+        fh.setFormatter(formatter);  
+
+        // the following statement is used to log any messages  
+        logger.info("Greetings from Spring Boot!");  
+
+    	
         return "Greetings from Spring Boot!";
     }
 
