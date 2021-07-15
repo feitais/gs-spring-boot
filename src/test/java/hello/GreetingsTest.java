8fbe33f9b4c14cb0decd8d402ff78fd94490fe77
Alvaro Feitais <feitais@MacBook-Pro-de-Alvaro.local>
2021-07-15 13:25:42 -0300

    .

--- src/test/java/hello/GreetingsTest.java
+++ src/test/java/hello/GreetingsTest.java
@@ -10,7 +10,7 @@ public class GreetingsTest {
 	@Test
 	public void test() throws URISyntaxException {
 	     
-	    final String baseUrl = "http://localhost:8080/";
+	    //final String baseUrl = "http://localhost:8080/";
 	 
 	    //Verify request succeed
 	    Assert.assertEquals(200, 200);
