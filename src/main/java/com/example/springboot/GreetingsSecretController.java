ee0fdf871b20b73b83f9b1a45c58f120267e4c56
alfeitais <alvaro.feitais@thoughtworks.com>
2024-01-03 09:52:39 -0300

    Implement secret manager #6

--- src/main/java/com/example/springboot/GreetingsSecretController.java
+++ src/main/java/com/example/springboot/GreetingsSecretController.java
@@ -1,6 +1,7 @@
 package com.example.springboot;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +9,8 @@ public class GreetingsSecretController {
     @Value("${sm://gs-secret-test}")
     String secret;
 
+    @GetMapping("/secret")
+	public String secret() {
+		return "Greetings from Spring Boot!! " + secret;
+	}
 }
